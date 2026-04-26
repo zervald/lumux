cc2e3d37234a724e3c34eb7ad1338e0c501510e6
Richard Demers <richardd1894@gmail.com>
2026-04-25 21:59:37 -0400

    tentative working flake
    Should work, but needs testing on different setups (nixos, non-nixos,
    etc.)

---
+++ flake.nix
@@ -0,0 +1,139 @@
+{
+  description = "Lumux";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        ## Import nixpkgs:
+        pkgs = import nixpkgs { inherit system; };
+
+        ## Read pyproject.toml file:
+        pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+
+        ## Get project specification:
+        project = pyproject.project;
+
+        ## Get the package:
+        package = pkgs.python3Packages.buildPythonPackage {
+          ## Set the package name:
+          pname = project.name;
+
+          ## Inherit the package version:
+          inherit (project) version;
+
+          ## Set the package format:
+          format = "pyproject";
+
+          ## Set the package source:
+          src = ./.;
+
+          ## Specify the build system to use:
+          build-system = with pkgs.python3Packages; [
+            setuptools
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            gtk4
+            libadwaita
+            xdg-desktop-portal
+            gobject-introspection
+            wrapGAppsHook4
+          ];
+
+          ## Specify production dependencies:
+          propagatedBuildInputs = with pkgs; [
+            openssl
+            python3Packages.gst-python
+            python3Packages.numpy
+            python3Packages.pillow
+            python3Packages.pydbus
+            python3Packages.pygobject3
+            python3Packages.requests
+            python3Packages.urllib3
+            python3Packages.zeroconf
+          ];
+
+          ## Specify test dependencies:
+          nativeCheckInputs = with pkgs; [
+            ## Python dependencies:
+            python3Packages.pytest
+            python3Packages.pytest-asyncio
+            python3Packages.ruff
+          ];
+
+          ## Define the check phase:
+          checkPhase = ''
+            runHook preCheck
+            ## Run tests here. For example:
+            # ${pkgs.python3.interpreter} -m pytest 
+            runHook postCheck
+          '';
+
+          meta.mainProgram = "lumux";
+        };
+
+        ## Make our package editable:
+        editablePackage = pkgs.python3.pkgs.mkPythonEditablePackage {
+          pname = project.name;
+          inherit (project) scripts version;
+          root = "$PWD";
+        };
+      in
+      {
+        ## Project packages output:
+        packages = {
+          "${project.name}" = package;
+          default = self.packages.${system}.${project.name};
+        };
+
+        ## Project development shell output:
+        devShells = {
+          default = pkgs.mkShell {
+            inputsFrom = [
+              package
+            ];
+
+            buildInputs = [
+              #################
+              ## OUR PACKAGE ##
+              #################
+
+              editablePackage
+
+              #################
+              # VARIOUS TOOLS #
+              #################
+
+              pkgs.python3Packages.build
+              pkgs.python3Packages.ipython
+              pkgs.python3Packages.ruff
+
+              ####################
+              # EDITOR/LSP TOOLS #
+              ####################
+
+              # LSP server:
+              pkgs.python3Packages.python-lsp-server
+
+              # LSP server plugins of interest:
+              pkgs.python3Packages.pylsp-mypy
+              pkgs.python3Packages.pylsp-rope
+              pkgs.python3Packages.python-lsp-ruff
+            ];
+          };
+        };
+      }
+    );
+}
